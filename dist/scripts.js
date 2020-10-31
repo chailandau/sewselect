75780455e70023c6eb630ea3ac55e7ea9195075f
Chai Landau <chai.landau@gmail.com>
2020-10-31 16:52:40 -0400

    Adds structure, mobile menu functionality

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,9 +1,11 @@
 // Mobile nav toggle classes on click
 let menuToggle = document.querySelector("#nav_toggle");
-let nav = document.querySelector("nav")
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
 
 menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active");
     nav.classList.toggle("active");
+    body.classList.toggle("no_scroll");
 })
 
